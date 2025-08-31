cadfe4d916d8e124ceeea8825195dd34822b2012
Yacobel <yacobelhaouarii@gmail.com>
2025-08-31 09:14:20 -0700

    update Card and inhace style

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,21 +6,23 @@ interface Iprops {
   alt: string;
   title: string;
   des: string;
-  price: number;
+  price: string;
   category: string;
 }
 
 function Card({ image, title, des, price, category, alt }: Iprops) {
   return (
     <>
-      <div className="grid mt-10 grid-cols-1 border rounded-md p-2">
+      <div className="flex flex-col justify-between max-w-sm mx-auto mt-10 border rounded-md p-2 md:max-w-lg">
         <Image
           src={image}
           classN="object-cover w-[100%] h-[200px] rounded-lg"
           alt={alt}
         />
         <h3 className="text-lg capitalize text-start mt-2">{title}</h3>
-        <p className=" capitalize text-start mt-4 text-gray-400">{des}</p>
+        <p className=" capitalize text-start mt-4 text-gray-400">
+          {`${des.slice(0, 150)}...`}
+        </p>
         <div className="flex justify-start gap-2 mt-4 items-center">
           <div className="w-5 h-5 rounded-full bg-amber-400"></div>
           <div className="w-5 h-5 rounded-full bg-amber-500"></div>
@@ -39,7 +41,14 @@ function Card({ image, title, des, price, category, alt }: Iprops) {
           </div>
         </div>
         <div className="flex justify-between gap-4 items-center mt-5">
-          <Button className=" bg-blue-800">edit</Button>
+          <Button
+            className=" bg-blue-800"
+            onClick={() => {
+              console.log("clicked");
+            }}
+          >
+            edit
+          </Button>
           <Button className=" bg-red-800">destroy</Button>
         </div>
       </div>
--- src/components/Ui/Buttone.tsx
+++ src/components/Ui/Buttone.tsx
@@ -1,15 +1,17 @@
-import type { ReactNode } from "react";
+import type { ButtonHTMLAttributes ,ReactNode } from "react";
 
-interface Iprops {
+interface Iprops extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   className: string;
+  
 }
 
-function Button({ children, className }: Iprops) {
+function Button({ children, className, ...rest }: Iprops) {
   return (
     <>
       <button
         className={`${className} w-[50%] text-white capitalize font-bold py-2 rounded-md `}
+        {...rest}
       >
         {children}
       </button>
